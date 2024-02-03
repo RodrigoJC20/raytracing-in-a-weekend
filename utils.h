9c6d37c67be6cbe95dcad1fecd6c62b204af5294
Rodrigo Jimenez <rjc.2002vmx@gmail.com>
2024-02-03 16:47:15 -0600

    Initial commit

---
+++ utils.h
@@ -0,0 +1,31 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <cmath>
+#include <limits>
+#include <memory>
+
+// Usings
+
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+// Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double PI = 3.1415926535897932385;
+
+// Utility Functions
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * PI / 180.0;
+}
+
+// Common headers
+
+#include "interval.h"
+#include "ray.h"
+#include "vec3.h"
+
+#endif
